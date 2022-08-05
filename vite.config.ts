3b0dec135da7de6746e470097ebe351756ffcb6b
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2022-08-05 13:16:53 -0400

    lint(repo): add remaining files.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+    build: {
+        watch: {
+            include: 'src/**/*',
+            exclude: ['**/node_modules/**', '**/*.spec.ts'],
+        },
+    },
+    server: {
+        port: 3000,
+    },
     test: {
+        cache: { dir: '.cache/vitest' },
+        restoreMocks: true,
+        coverage: {
+            reporter: ['text', 'json', 'html'],
+        },
         environment: 'jsdom',
+        include: ['**/*.spec.ts'],
         globals: true,
+        reporters: 'default',
+        root: 'src',
+        // setupFiles: [''],
     },
 });
