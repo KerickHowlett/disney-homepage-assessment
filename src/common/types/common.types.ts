8dfba163a68de22402b21e83200ee0a2a1b28b12
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2022-08-18 14:39:55 -0400

    chore(common): remove unneeded typing

--- src/common/types/common.types.ts
+++ src/common/types/common.types.ts
@@ -9,17 +9,7 @@ export type ComponentParams = {
     selector: string;
     options?: ElementDefinitionOptions;
 };
-export interface CustomElement {
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    constructor: Function & {
-        observedAttributes?: string[];
-    };
-    rootHtml: RootHTML;
-    attributeChangedCallback?(attributeName: string, oldValue: string, newValue: string): void;
-    connectedCallback?(): void;
-    disconnectedCallback?(): void;
-    render(): void;
-}
+
 export type ComponentDecoratorFunc = (target: CustomElementConstructor) => void;
 export type InternalStoreEvents = Map<string, ReadonlyArray<Callback>>;
 export type PubSubEvents = ReadonlyMap<string, ReadonlyArray<Callback>>;
