20fd20213f77a5547a81a96a64e76868c947a005
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2022-08-18 15:38:25 -0400

    feat(common): allow for image src error handling

--- src/common/ui/image/image.component.ts
+++ src/common/ui/image/image.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@common/decorators';
+import { isNull } from '@common/utils';
 
 @Component({
     selector: 'disney-image',
+    options: { extends: 'img' },
 })
 export class ImageComponent extends HTMLImageElement {
     constructor() {
         super();
+    }
+
+    private readonly failsafeImage: string | null = this.getAttribute('failsafe-src');
+    private readonly failsafeTitle: string | null = this.getAttribute('alt');
+
+    connectedCallback(): void {
+        this.onerror = this.renderFailsafeImage.bind(this);
+    }
+
+    renderFailsafeImage(): void {
+        if (isNull(this.failsafeImage) || isNull(this.failsafeTitle)) return;
+
+        this.src = this.failsafeImage;
+        const titleElement = document.createElement('div');
+        titleElement.classList.add('image-failsafe-title', 'text-color--primary');
+        titleElement.textContent = this.failsafeTitle;
+        this.insertAdjacentElement('afterend', titleElement);
     }
 }
