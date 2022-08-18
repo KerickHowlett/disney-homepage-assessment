40f3c796211e12f9759c1c74f6ba946e7d4934fd
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2022-08-18 19:57:37 -0400

    style(home): clean up code a bit

--- src/common/utils/merge/merge.utils.ts
+++ src/common/utils/merge/merge.utils.ts
@@ -1,5 +1,4 @@
 export function merge<TReturn extends Record<PropertyKey, unknown> = Record<PropertyKey, unknown>>(
-    // eslint-disable-next-line @typescript-eslint/ban-types
     ...objects: Record<PropertyKey, unknown>[]
 ): TReturn {
     return Object.assign({}, ...objects);
--- src/features/home/helpers/get-containers/get-containers.helpers.ts
+++ src/features/home/helpers/get-containers/get-containers.helpers.ts
@@ -1,5 +1,5 @@
 import type { Container, HomeAPIResponse } from '../../types';
 
-export function getContainers(response: HomeAPIResponse | null): ReadonlyArray<Container> {
-    return response?.data.StandardCollection.containers ?? [];
+export function getContainers(response: HomeAPIResponse | null): Container[] {
+    return (response?.data.StandardCollection.containers as Container[]) ?? [];
 }
