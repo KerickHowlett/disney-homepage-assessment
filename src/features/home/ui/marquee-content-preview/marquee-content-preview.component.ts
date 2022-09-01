50455a0f69eb198d17a0d9b0236e864f4473dfc7
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2022-09-01 12:54:43 -0400

    feat(home): add better error handling for content preview

--- src/features/home/ui/marquee-content-preview/marquee-content-preview.component.ts
+++ src/features/home/ui/marquee-content-preview/marquee-content-preview.component.ts
@@ -1,4 +1,4 @@
-import { changeDetectedBetween, Component } from '@disney/common';
+import { changeDetectedBetween, Component, isEmpty } from '@disney/common';
 
 import css from './marquee-content-preview.component.css?inline';
 
@@ -33,8 +33,7 @@ export class MarqueeContentPreviewComponent extends HTMLElement {
         if (!changeDetectedBetween(oldValue, newValue)) return;
         switch (name) {
             case 'image':
-                this.videoElement.poster = newValue;
-                break;
+                return this.onPosterSrcChange(newValue);
             case 'video':
                 return this.onVideoSrcChange(newValue);
         }
@@ -42,6 +41,7 @@ export class MarqueeContentPreviewComponent extends HTMLElement {
 
     connectedCallback(): void {
         this.render();
+        this.bindVideoEventHandlers();
     }
 
     render(): void {
@@ -50,13 +50,45 @@ export class MarqueeContentPreviewComponent extends HTMLElement {
             <video id="video-preview" class="video-preview" muted loop>
                 <source type="video/mp4">
             </video>
+            <img
+                alt="Video Preview"
+                aria-label="Video Preview"
+                class="image-fallback video-preview"
+                is="disney-image"
+                id="image-fallback"
+                loading="lazy"
+                style="display: none;"
+            />
         `;
     }
 
+    private bindVideoEventHandlers(): void {
+        this.videoElement.onerror = this.onError.bind(this);
+        this.videoElement.onloadeddata = this.onSuccess.bind(this);
+    }
+
+    // @TODO: Add fade transition to hide video and show poster image, as well
+    //        as means to replace video element with image of poster for error
+    //        handling.
     private onVideoSrcChange(videoSrc: string): void {
-        this.videoElement.pause();
+        if (isEmpty(videoSrc)) return this.onError();
         this.videoSourceElement.setAttribute('src', videoSrc);
         this.videoElement.load();
-        this.videoElement.play().catch(() => this.videoElement.pause());
+    }
+
+    private onError(): void {
+        this.videoElement.style.display = 'none';
+        this.imageElement.style.display = 'block';
+    }
+
+    private onPosterSrcChange(posterSrc: string): void {
+        this.imageElement.src = posterSrc;
+        this.videoElement.poster = posterSrc;
+    }
+
+    private onSuccess(): void {
+        this.videoElement.style.display = 'block';
+        this.imageElement.style.display = 'none';
+        this.videoElement.play().catch(this.videoElement.pause.bind(this));
     }
 }
